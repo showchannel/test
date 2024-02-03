8a35fffff8829df10255209cd53d7c31bb423eaf
ishinaka <73539563+showchannel@users.noreply.github.com>
2024-02-03 14:18:46 +0900

    Add files via upload

---
+++ study/src/study/GameMain.java
@@ -0,0 +1,22 @@
+package study;
+
+// ゲームメインクラス
+public class GameMain {
+	public static void main(String[] args) {
+		// 山札を生成
+		Deck deck = new Deck();
+
+		// 親がカードを引く
+		Player parent = new Player();
+		parent.Draw(deck);
+
+		// 子がカードを引く
+		Player child = new Player();
+		child.Draw(deck);
+
+		// ゲーム画面を生成
+		new Display(parent, child);
+
+		return;
+	}
+}
